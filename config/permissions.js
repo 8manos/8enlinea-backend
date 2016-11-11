704e98dab8449a90cf309b802fe5fb4ffa25935a
Anabelle Handdoek <ana@gathacol.net>
2016-11-11 11:36:20 -0500

    modify gitignore

--- config/permissions.js
+++ config/permissions.js
@@ -6,6 +6,5 @@ var _super = require('sails-permissions/config/permissions');
 _.merge(exports, _super);
 _.merge(exports, {
 
-  // Extend with custom logic here by adding additional fields, methods, etc.
 
 });
