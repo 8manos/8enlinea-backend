2f826ba876af5775dd3a8b6248a911f04835b793
Anabelle Handdoek <ana@gathacol.net>
2016-11-24 22:19:32 -0500

    conversacion find

--- api/controllers/ConversacionController.js
+++ api/controllers/ConversacionController.js
@@ -6,6 +6,22 @@
  */
 
 module.exports = {
+	find: function( req, res ){	
+		Conversacion.findOne( req.param('id') ).populate('mensajes').populate('usuarios').populate('alternativas').exec(function (err, conversacion) {
+		  if (err) { return; }
+		    var data = new Object();
+      		data.conversacion = conversacion;
+
+		  	if (req.wantsJSON) {
+				return res.json( {data} );
+			}
+			else {
+				return res.view( {data} );
+			}
+
+		});
+	},
+
 	responder: function (req, res) {
 		sails.log( 'El usuario ' + req.session.id + 'ha solicitado el destino: ', req.param('destino') );
 		Plantilla.find({ identificador: req.param('destino') }).populate('respuestas').populate('acciones').exec(function (err, plantilla) {
