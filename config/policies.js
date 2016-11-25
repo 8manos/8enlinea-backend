74e86d5fb64ee71750f868125bc0af8b291a5be2
Anabelle Handdoek <ana@gathacol.net>
2016-11-24 19:44:38 -0500

    allow logout

--- config/policies.js
+++ config/policies.js
@@ -39,7 +39,8 @@ module.exports.policies = {
   ],
 
   AuthController: {
-    '*': [ 'passport' ]
+    '*': [ 'passport' ],
+    logout: true
   },
 
   /***************************************************************************
